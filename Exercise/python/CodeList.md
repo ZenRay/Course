cb75475124f526468ca39a8223286df193338b26
ZenRay <jake2011ren@gmail.com>
2018-03-04 22:50:04 +0800

    sort the value by insertion method

--- Exercise/python/CodeList.md
+++ Exercise/python/CodeList.md
@@ -5,8 +5,16 @@
 ## divisors
 返回数字的被除数，如果是质数那么返回质数
 
-## alphabet_position
+## alphabet\_position
 返回字符串中字符在 26 个字母中的绝对位置，Aa-Zz 分别对应 1-26
 
-## alphabet_position
+## alphabet\_position
 返回字符串中奇数索引位上字符大写
+
+## to\_weird\_case
+对奇数位上的字符进行大写
+
+## tower\_builder
+返回特定高度的字符塔，该函数中使用了 format string syntax
+
+## order\_weight
--- Exercise/python/codewar.py
+++ Exercise/python/codewar.py
@@ -95,6 +95,63 @@ def tower_builder(n_floors):
 
     for i in range(0, n_floors):
         result.append('{0:{fill}{align}{length}}'.format(
-            "*" * (2 * i - 1), ill=" ", align="^", length=length))
+            "*" * (2 * i - 1), fill=" ", align="^", length=length))
 
     return result
+
+
+"""
+ problem:
+    "56 65 74 100 99 68 86 180 90" ordered by numbers weights becomes:
+    "100 180 90 56 65 74 68 86 99" When two numbers have the same "weight",
+    let us class them as if they were strings and not numbers: 100 is before
+    180 because its "weight" (1) is less than the one of 180 (9) and 180 is
+    before 90 since, having the same "weight" (9) it comes before as a string.
+    All numbers in the list are positive numbers and the list can be empty
+ blueprint:
+    use the insertion method to scan the sorted list and update the list
+
+"""
+
+
+def check(string, value):
+    # check whether the strings are inserted
+    for char1, char2 in zip(string, value):
+        if int(char1) > int(char2):
+            return True
+        elif int(char1) < int(char2):
+            return False
+
+    if len(string) >= len(value):
+        return True
+    else:
+        return False
+
+
+def insertion(array, value):
+    # parameter: the value need to be updated; the array is sorted list
+    for i in range(0, len(array)):
+        if sum([int(x) for x in array[i]]) == sum([int(x) for x in value]):
+            if check(array[i], value):
+                array.insert(i, value)
+                return
+    array.append(value)
+
+
+def order_weight(s: str):
+    # parameter: s is a string with list seperated by space
+    weight = s.split(" ")
+
+    weight.sort(key=lambda x: sum([int(i) for i in x]))
+
+    result = []
+    result.append(weight[0])
+
+    for i in range(1, len(weight)):
+        insertion(result, weight[i])
+
+    return " ".join(result)
+
+
+print(order_weight("71899703 200 6 91 425 4 67407 7 96488 6 4 2 7 31064 9 7920 \
+    1 34608557 27 72 18 81"))
