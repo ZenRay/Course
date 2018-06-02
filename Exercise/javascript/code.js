1ddd56b14fb7e3ca6f039c322f3958e584d7b01b
ZenRay <jake2011ren@gmail.com>
2018-06-02 11:31:44 +0800

    add the materialnote directory

--- Exercise/javascript/code.js
+++ Exercise/javascript/code.js
@@ -6,11 +6,29 @@
  * @param {integer} p - the most left index
  * @param {integer} r - the most right index
  * @return {integer} i - the pivot elemnt index after sorting
-*/
-var partition=function(array, p, r){
-    // This code has been purposefully obfuscated,
-    // as you will implement it yourself in next challenge
-    var e=array,t=p,n=r;var r=function(e,t,n){var r=e[t];e[t]=e[n];e[n]=r;};var i=t;for(var s=t;s<n;s++){if(e[s]<=e[n]){r(e,s,i);i++;}}r(e,n,i);return i;
+ */
+var partition = function(array, p, r) {
+  // This code has been purposefully obfuscated,
+  // as you will implement it yourself in next challenge
+  var e = array,
+    t = p,
+    n = r;
+  var r = function(e, t, n) {
+    var r = e[t];
+    e[t] = e[n];
+    e[n] = r;
+  };
+
+  var i = t;
+
+  for (var s = t; s < n; s++) {
+    if (e[s] <= e[n]) {
+      r(e, s, i);
+      i++;
+    }
+  }
+  r(e, n, i);
+  return i;
 };
 
 /**
@@ -18,12 +36,12 @@ var partition=function(array, p, r){
  * @param {array} array - given a array
  * @param {integer} p - the most left index
  * @param {integer} r - the most right index
-*/
+ */
 
 var quickSort = function(array, p, r) {
-    if (p < r) {
-        var i = partition(array, p, r);
-        quickSort(array, p, i-1);
-        quickSort(array, i+1, r);
-    }
+  if (p < r) {
+    var i = partition(array, p, r);
+    quickSort(array, p, i - 1);
+    quickSort(array, i + 1, r);
+  }
 };
--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 针对已经学习的课程，整理出的相关笔记、课程中的代码以及其他练习代码
 
 # 文件目录
-1. AlgorithmicToolbox： Coursera 中专项课程 [Data Structures and Algorithms ](https://www.coursera.org/specializations/data-structures-algorithms) 的第一个课程
-2. Exercise：在不同地方练习中总结的代码
-3. IntrotoAlgorithms：Udacity 中介绍算法的课程—— [Intro to Algorithms](https://classroom.udacity.com/courses/cs215)
+1. [AlgorithmicToolbox](./AlgorithmicToolbox)： Coursera 中专项课程 [Data Structures and Algorithms ](https://www.coursera.org/specializations/data-structures-algorithms) 的第一个课程
+2. [Exercise](./Exercise)：在不同地方练习中总结的代码
+3. [IntrotoAlgorithms](./IntrotoAlgorithms)：Udacity 中介绍算法的课程—— [Intro to Algorithms](https://classroom.udacity.com/courses/cs215)
+4. [MaterialNote](./MaterialNote)：包括阅读的材料的笔记整理
